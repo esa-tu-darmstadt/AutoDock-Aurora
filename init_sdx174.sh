5ebabefb3c937637ca7cab907dcf05bc7b567b95
Leonardo Solis <solis@esa.informatik.tu-darmstadt.de>
2018-10-31 14:53:46 +0100

    corrected sdx174 initialization file

--- init_sdx174.sh
+++ init_sdx174.sh
@@ -5,8 +5,10 @@ echo "Setting up Xilinx SDAccel SDK"
 echo "============================="
 echo ""
 
-echo export SDX174_LOCAL_INITFILE=/opt/cad/xilinx/sdx/SDx/2017.4/settings64.sh
-echo source $SDX174_LOCAL_INITFILE
+export SDX174_LOCAL_INITFILE=/opt/cad/xilinx/sdx/SDx/2017.4/settings64.sh
+echo "Initialization file: " $SDX174_LOCAL_INITFILE
+echo " "
+source $SDX174_LOCAL_INITFILE
 
 echo "Xilinx SDAccel version: "
 sdx --version
