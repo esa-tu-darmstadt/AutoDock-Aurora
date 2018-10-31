a9ca7077afbe57887836d583cf0169bffda9be96
Leonardo Solis <solis@esa.informatik.tu-darmstadt.de>
2018-10-31 16:05:28 +0100

    added local or remote configs to init file

--- init_sdx174.sh
+++ init_sdx174.sh
@@ -5,11 +5,38 @@ echo "Setting up Xilinx SDAccel SDK"
 echo "============================="
 echo ""
 
-export SDX174_LOCAL_INITFILE=/opt/cad/xilinx/sdx/SDx/2017.4/settings64.sh
-echo "Initialization file: " $SDX174_LOCAL_INITFILE
+# Asking user to specify server configuration
+echo "About tool installations:"
+echo "\"local\" server contains all tools for development."
+echo "\"remote\" server might be used only for evaluation."
 echo " "
-source $SDX174_LOCAL_INITFILE
+read -p "Enter \"local\" or \"remote\" for server to use: " serverconfig_userinput
 
-echo "Xilinx SDAccel version: "
-sdx --version
-echo " "
+# Checking user input
+if [ "$serverconfig_userinput" == "local" ]; then
+	echo "Server configuration selected: " $serverconfig_userinput
+elif [ "$serverconfig_userinput" == "remote" ]; then
+	echo "Server configuration selected: " $serverconfig_userinput
+else
+	serverconfig_userinput="local"
+ 	echo "This is not a valid server configuration. Default \"local\" is selected."
+fi
+
+# Selecting path for settings64.sh
+if [ "$serverconfig_userinput" == "local" ]; then
+	SDX174_INITFILE=/opt/cad/xilinx/sdx/SDx/2017.4/settings64.sh
+elif [ "$serverconfig_userinput" == "remote" ]; then
+	SDX174_INITFILE=/opt/xilinx-2017.4/SDx/2017.4/settings64.sh
+fi
+
+# Verifying that settings64.sh file exists
+# If it does, only then initialize SDx-v2017.4
+if [ ! -f "$SDX174_INITFILE" ]; then
+	echo "File $SDX174_INITFILE not found!"
+else
+	echo "Initialization file in $serverconfig_userinput server:" $SDX174_INITFILE
+	source "$SDX174_INITFILE"
+	echo " "
+	echo "Xilinx SDAccel version: "
+	sdx --version
+fi
