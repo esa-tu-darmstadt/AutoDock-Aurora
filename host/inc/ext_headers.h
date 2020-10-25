21cb2972ea12209d2b8858d3bb59bc913af63856
Leonardo Solis V <leonardosolis86@gmail.com>
2020-10-25 15:17:20 +0100

    Commenting out inclusion of header xcl2.hpp

--- host/inc/ext_headers.h
+++ host/inc/ext_headers.h
@@ -5,7 +5,9 @@
 
 #include <math.h>	// calcenergy.h, getparameters.h, miscellaneous.h, processgrid.h, processresult.h
 #include <stdio.h>	// calcenergy.h, getparameters.h, miscellaneous.h, processgrid.h, processresult.h, performdocking.h
+/*
 #include "xcl2.hpp"	// calcenergy.h, performdocking.h
+*/
 #include <string.h>	// getparameters.h, processgrid.h, processresult.h
 #include <time.h>	// miscellaneous.h, performdocking.h, main.cpp
 #include <stdlib.h>	// miscellaneous.h, performdocking.h, processgrid.h
