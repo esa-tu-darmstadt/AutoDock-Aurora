e4f991a4aee7ffe71bb8820a1407fdf11728d8c5
Leonardo Solis V <leonardosolis86@gmail.com>
2020-11-19 22:47:03 +0100

    Reordering code

--- device/calc_pc.c
+++ device/calc_pc.c
@@ -13,9 +13,9 @@ void calc_pc (
 	const	float*	restrict	PC_ref_coords_z,
 	const	float*	restrict	PC_rotbonds_moving_vectors,
 	const	float*	restrict	PC_rotbonds_unit_vectors,
+	const	float*	restrict	PC_ref_orientation_quats,
 			uint				DockConst_rotbondlist_length,
 			uchar				DockConst_num_of_genes,
-	const	float*	restrict	PC_ref_orientation_quats,
 			ushort				Host_RunId,
 
 	const	float*	restrict	genotype,
--- device/energy_ia.c
+++ device/energy_ia.c
@@ -9,20 +9,16 @@
 void energy_ia (
 	const	float*	restrict	IA_IE_atom_charges,
  	const	char*	restrict	IA_IE_atom_types,
-
 	const	char*	restrict	IA_intraE_contributors,
-
-			float				DockConst_smooth,
 	const	float*	restrict	IA_reqm,
 	const	float*	restrict	IA_reqm_hbond,
 	const	uint*	restrict	IA_atom1_types_reqm,
 	const	uint*	restrict	IA_atom2_types_reqm,
-
 	const	float*	restrict	IA_VWpars_AC,
 	const	float*	restrict	IA_VWpars_BD,
 	const	float*	restrict	IA_dspars_S,
  	const	float*	restrict	IA_dspars_V,
-
+	 		float				DockConst_smooth,
 			uint				DockConst_num_of_intraE_contributors,
 			float				DockConst_grid_spacing,
 			uchar				DockConst_num_of_atypes,
