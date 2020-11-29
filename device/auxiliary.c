e629cd270f846328b0e8a1df60601c3de288a91d
Leonardo Solis V <leonardosolis86@gmail.com>
2020-11-29 13:17:09 +0100

    RE-implementing using loops for esa_dot3() and esa_dot4()

--- device/auxiliary.c
+++ device/auxiliary.c
@@ -93,14 +93,26 @@ float randf(unsigned int* input) {
  * -----------------------------------------------
  * */
 
-// TODO: can be vectorized with a loop ?
 float esa_dot3(float a[3], float b[3]) {
-	return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2]);
+
+	//return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2]);
+
+	float tmp[3];
+	for (uint i = 0; i < 3; i++) {
+		tmp[i] = a[i] * b[i];
+	}
+	return (tmp[0] + tmp[1] + tmp[2]);
 }
 
-// TODO: can be vectorized with a loop ?
 float esa_dot4(float a[4], float b[4]) {
-	return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3]);
+	
+	//return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3]);
+
+	float tmp[4];
+	for (uint i = 0; i < 4; i++) {
+		tmp[i] = a[i] * b[i];
+	}
+	return (tmp[0] + tmp[1] + tmp[2] + tmp[3]);
 }
 
 /* 
