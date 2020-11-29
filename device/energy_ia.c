cc032f4b04feebd77cdbea19058453be70bca6b1
Leonardo Solis V <leonardosolis86@gmail.com>
2020-11-29 13:55:11 +0100

    IA: further attempt to reduce overhead of loop division

--- device/energy_ia.c
+++ device/energy_ia.c
@@ -120,6 +120,7 @@ void energy_ia (
 		float inverse_smoothed_distance_pow_12 = inverse_smoothed_distance_pow_6 * inverse_smoothed_distance_pow_6;
 
 		float term_partialE3 = atomic_distance * (-8.5525f + (86.9525f / (1.0f + 7.7839f * exp(-0.3154f * atomic_distance))));
+		float term_inv_partialE3 = (1.0f / term_partialE3);
 
 		// Calculating energy contributions
 		// Cuttoff1: internuclear-distance at 8A only for vdw and hbond.
@@ -141,7 +142,7 @@ void energy_ia (
 		if (atomic_distance < 20.48f)
 		{
 			// Calculating electrostatic term
-			partialE3 += (DockConst_coeff_elec * IA_IE_atom_charges[atom1_id] * IA_IE_atom_charges[atom2_id]) /  term_partialE3;
+			partialE3 += DockConst_coeff_elec * IA_IE_atom_charges[atom1_id] * IA_IE_atom_charges[atom2_id] * term_inv_partialE3;
 
 			// Calculating desolvation term
 			partialE4 += (
