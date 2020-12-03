753d5aa6292c3e883cbdba8d23ccfea2e6dd7a78
Leonardo Solis V <leonardosolis86@gmail.com>
2020-12-04 00:15:56 +0100

    Updating libhello for tracing, but using stack args

--- device/libhello.c
+++ device/libhello.c
@@ -12,79 +12,119 @@ uint64_t hello()
   return 0;
 }
 
+typedef struct
+{
+	  float			    atom_charges_const	    	  [MAX_NUM_OF_ATOMS];
+    int  			    atom_types_const  	     	  [MAX_NUM_OF_ATOMS];
+	  int				    intraE_contributors_const   [3*MAX_INTRAE_CONTRIBUTORS];
+    float 			  reqm_const 		     		      [ATYPE_NUM];
+    float 			  reqm_hbond_const 	     	    [ATYPE_NUM];
+    unsigned int 	atom1_types_reqm_const 	     [ATYPE_NUM];
+    unsigned int	atom2_types_reqm_const 	     [ATYPE_NUM];
+    float     		VWpars_AC_const              [MAX_NUM_OF_ATYPES*MAX_NUM_OF_ATYPES];
+    float     		VWpars_BD_const              [MAX_NUM_OF_ATYPES*MAX_NUM_OF_ATYPES];
+    float     		dspars_S_const               [MAX_NUM_OF_ATYPES];
+    float     		dspars_V_const               [MAX_NUM_OF_ATYPES];
+    int       		rotlist_const                [MAX_NUM_OF_ROTATIONS];
+    float 			  ref_coords_x_const			 [MAX_NUM_OF_ATOMS];
+	  float 			  ref_coords_y_const			 [MAX_NUM_OF_ATOMS];
+	  float 			  ref_coords_z_const			 [MAX_NUM_OF_ATOMS];
+    float			    rotbonds_moving_vectors_const[3*MAX_NUM_OF_ROTBONDS];
+    float 			  rotbonds_unit_vectors_const  [3*MAX_NUM_OF_ROTBONDS];
+	  float 			  ref_orientation_quats_const  [4*MAX_NUM_OF_RUNS];
+} kernelconstant_static;
+
 int main() {
   printf("This is main()\n");
   hello();
 
-/*
+
   // Using real VE VMA addresses for artifially invoking kernel2
   
   // Defining and initializing structs
   // as local variables -> will be stored in the stack!
   // This mimic what we did in our VEOffload design
   // as we passed some into the stack.
-  // Initilization eference: host/src/calcenergy.cpp
+  // Initilization reference: host/src/calcenergy.cpp
+
+  // init populations
+  float cpu_init_populations[MAX_NUM_OF_RUNS][MAX_POPSIZE][ACTUAL_GENOTYPE_LENGTH];
+
+  for (int i = 0; i < MAX_NUM_OF_RUNS; i++) {
+    for (int j = 0; j < MAX_POPSIZE; j++) {
+      for (int k = 0; k < ACTUAL_GENOTYPE_LENGTH; k++) {
+        cpu_init_populations[i][j][k] = 10.0f;
+      }
+    }
+  }
+  
+  // prng
+  unsigned int cpu_prng_seeds[MAX_NUM_OF_RUNS][MAX_POPSIZE];
+
+  for (int i = 0; i < MAX_NUM_OF_RUNS; i++) {
+    for (int j = 0; j < MAX_POPSIZE; j++) {
+      cpu_prng_seeds[i][j] = 30;
+    }
+  }
+
   int m;
+  kernelconstant_static KerConstStatic;
 
-  kernelconstant_interintra K_interintra;
-  for (m = 0; m < MAX_NUM_OF_ATOMS; m++) { 
-    K_interintra.atom_charges_const[m] = (float) 0.1f; 
-    K_interintra.atom_types_const[m] = (char) 5;
-  }
-
-  kernelconstant_intracontrib K_intracontrib;
-  for (m = 0; m < 3*MAX_INTRAE_CONTRIBUTORS; m++) { 
-    K_intracontrib.intraE_contributors_const[m] = (char) 5; 
-  }
-
-  kernelconstant_intra K_intra;
-  for (m = 0; m < ATYPE_NUM; m++)	{ 
-    K_intra.reqm_const[m] = (float) 0.1f; 
-    K_intra.reqm_hbond_const[m] = (float) 0.1f;
-    K_intra.atom1_types_reqm_const[m] = (unsigned int) 5;
-    K_intra.atom2_types_reqm_const[m] = (unsigned int) 5;
-  }
-	for (m=0;m<MAX_NUM_OF_ATYPES*MAX_NUM_OF_ATYPES;m++)	{ 
-    K_intra.VWpars_AC_const[m] = (float) 0.1f;
-    K_intra.VWpars_BD_const[m] = (float) 0.1f;
-  }
-	for (m=0;m<MAX_NUM_OF_ATYPES;m++) {
-    K_intra.dspars_S_const[m] = (float) 0.1f; 
-    K_intra.dspars_V_const[m] = (float) 0.1f;
-  }
-
-	kernelconstant_rotlist K_rotlist;
   for (m = 0; m < MAX_NUM_OF_ROTATIONS; m++) { 
-    K_rotlist.rotlist_const[m] = (int) 5; 
+    KerConstStatic.rotlist_const[m] = (int) 5; 
   }
 
-	kernelconstant_conform K_conform;
   for (m = 0; m < MAX_NUM_OF_ATOMS; m++) { 
-    K_conform.ref_coords_x_const[m]	= (float) 0.1f; 
-    K_conform.ref_coords_y_const[m]	= (float) 0.1f; 
-    K_conform.ref_coords_z_const[m] = (float) 0.1f;
+    KerConstStatic.ref_coords_x_const[m] = (float) 0.1f; 
+    KerConstStatic.ref_coords_y_const[m] = (float) 0.1f; 
+    KerConstStatic.ref_coords_z_const[m] = (float) 0.1f;
   }
+
 	for (m = 0; m < 3*MAX_NUM_OF_ROTBONDS; m++){ 
-    K_conform.rotbonds_moving_vectors_const[m] = (float) 0.1f;
-    K_conform.rotbonds_unit_vectors_const[m]  = (float) 0.1f;
+    KerConstStatic.rotbonds_moving_vectors_const[m] = (float) 0.1f;
+    KerConstStatic.rotbonds_unit_vectors_const[m]  = (float) 0.1f;
   }
+
 	for (m = 0; m < 4*MAX_NUM_OF_RUNS; m++) { 
-    K_conform.ref_orientation_quats_const[m] = (float) 0.1f;
+    KerConstStatic.ref_orientation_quats_const[m] = (float) 0.1f;
   }
 
-*/
+  for (m = 0; m < MAX_NUM_OF_ATOMS; m++) { 
+    KerConstStatic.atom_charges_const[m] = (float) 0.1f; 
+    KerConstStatic.atom_types_const[m] = (char) 5;
+  }
 
+  for (m = 0; m < 3*MAX_INTRAE_CONTRIBUTORS; m++) { 
+    KerConstStatic.intraE_contributors_const[m] = (char) 5; 
+  }
+
+  for (m = 0; m < ATYPE_NUM; m++)	{ 
+    KerConstStatic.reqm_const[m] = (float) 0.1f; 
+    KerConstStatic.reqm_hbond_const[m] = (float) 0.1f;
+    KerConstStatic.atom1_types_reqm_const[m] = (unsigned int) 5;
+    KerConstStatic.atom2_types_reqm_const[m] = (unsigned int) 5;
+  }
+
+	for (m=0;m<MAX_NUM_OF_ATYPES*MAX_NUM_OF_ATYPES;m++)	{ 
+    KerConstStatic.VWpars_AC_const[m] = (float) 0.1f;
+    KerConstStatic.VWpars_BD_const[m] = (float) 0.1f;
+  }
+
+	for (m=0;m<MAX_NUM_OF_ATYPES;m++) {
+    KerConstStatic.dspars_S_const[m] = (float) 0.1f; 
+    KerConstStatic.dspars_V_const[m] = (float) 0.1f;
+  }
 
   #if defined (ENABLE_TRACE)
   ftrace_region_begin("OUTSIDE_LOOP_KERNEL_GA");
   #endif
   libkernel_ga(
-              105621851762448, // mem_dockpars_conformations_current_Initial
+              &cpu_init_populations[0][0][0],
               105621851785264, // mem_dockpars_conformations_current_Final
               105621851808080, // mem_dockpars_energies_current
               105621851808688, // mem_evals_performed
               105621851808720, // mem_gens_performed
-              105621851808752, // mem_prng_states
+              &cpu_prng_seeds[0][0],
               150, // dockpars.pop_size
               2048000, // dockpars.num_of_energy_evals
               99999, // dockpars.num_of_generations
@@ -98,26 +138,26 @@ int main() {
               150, // dockpars.num_of_lsentities
               8, // dockpars.num_of_genes
 
-              105621851809360, // mem_pc_rotlist_const
-              105621851825760, // mem_pc_ref_coords_x_const
-              105621851826800, // mem_pc_ref_coords_y_const
-              105621851827840, // mem_pc_ref_coords_z_const
-              105621851828880, // mem_pc_rotbonds_moving_vectors_const
-              105621851829280, // mem_pc_rotbonds_unit_vectors_const
-              105621851829680, // mem_pc_ref_orientation_quats_const
+              &KerConstStatic.rotlist_const[0],
+              &KerConstStatic.ref_coords_x_const[0],
+              &KerConstStatic.ref_coords_y_const[0],
+              &KerConstStatic.ref_coords_z_const[0],
+              &KerConstStatic.rotbonds_moving_vectors_const[0],
+              &KerConstStatic.rotbonds_unit_vectors_const[0,
+              &KerConstStatic.ref_orientation_quats_const[0],
               31, // dockpars.rotbondlist_length
 
-              105621868837424, // mem_ia_ie_atom_charges_const
-              105621868838464, // mem_ia_ie_atom_types_const
-              105621868838736, // mem_ia_contributors_const
-              105621868936288, // mem_ia_reqm_const
-              105621868936384, // mem_ia_reqm_hbond_const
-              105621868936480, // mem_ia_atom1_types_reqm_const
-              105621868936576, // mem_ia_atom2_types_reqm_const
-              105621868936672, // mem_ia_VWpars_AC_const
-              105621868937472, // mem_ia_VWpars_BD_const
-              105621868938272, // mem_ia_dspars_S_const
-              105621868938336, // mem_ia_dspars_V_const
+              &KerConstStatic.atom_charges_const[0],
+              &KerConstStatic.atom_types_const[0],
+              &KerConstStatic.intraE_contributors_const[0],
+              &KerConstStatic.reqm_const[0],
+              &KerConstStatic.reqm_hbond_const[0],
+              &KerConstStatic.atom1_types_reqm_const[0],
+              &KerConstStatic.atom2_types_reqm_const[0],
+              &KerConstStatic.VWpars_AC_const[0],
+              &KerConstStatic.VWpars_BD_const[0],
+              &KerConstStatic.dspars_S_const[0],
+              &KerConstStatic.dspars_V_const[0],
               0.5, // dockpars.smooth
               88, // dockpars.num_of_intraE_contributors
               0.375, // dockpars.grid_spacing
