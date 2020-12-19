e3ed5f9008e1a4d2a81af7bc2ee7d996e3e3c1f5
Erich Focht <efocht@gmail.com>
2020-12-19 15:08:09 +0100

    Adding PackBuff class that manages packing various data buffers into a large one, aimed at being transfered to the VE device. This should save multiple little transfers and facilitate passing device kernel args as a single struct. Drawback: empty buffers (aimed at transfering results out of the device) are currently also allocated on host side. This can be improved later.

---
+++ host/inc/packbuff.hpp
@@ -0,0 +1,91 @@
+#ifndef PACKBUFF_H_
+#define PACKBUFF_H_
+
+/*
+  Class that handles a buffer to be packed with
+  arrays of various sizes. The buffer shall be transfered
+  to an accelerator device (eg. VE).
+  Each array is allocated at an 8-byte aligned address.
+
+  The procedure consists of the steps:
+  - create a PackBuff() object
+  - add arrays to it by calling .pack()
+    - the data buffer grows as needed
+    - store offset at the fixup address which will contain the VE address
+  - get final size of buffer and allocate it on the VE
+  - fixup pointer addresses by adding the buffer's VE address to each
+    of the fixup addresses
+  - transfer the data buffer to the VE
+
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdexcept>
+#include <vector>
+
+#define ALIGN8B(x) (((uint64_t)(x) + 7UL) & ~7UL)
+
+
+class PackBuff {
+private:
+  char *data_;  // data of the packed buffer
+  size_t size_; // current size of the buffer
+  size_t size_alloc;  // allocated size of the buffer
+  size_t chunksz;  // minimum data buffer growth granularity
+  std::vector<void **> fixup_; // addresses that need to be "fixed" by adding the VE
+public:
+  PackBuff(size_t n) : chunksz(n), size_(0) {
+    data_ = (char *)malloc(chunksz);
+    if (!data_)
+      throw std::runtime_error("ERROR: failed to allocate initial PackBuff!");
+    size_alloc = chunksz;
+  }
+  ~PackBuff() {
+    if (data_)
+      free(data_);
+  }
+  void pack(void *buff, size_t len, void **address) {
+    size_t asize = ALIGN8B(size_);  // aligned pointer to buff destination
+    // realloc if needed
+    if (asize + len > size_alloc) {
+      size_t new_size = ((asize + len + chunksz - 1) / chunksz ) * chunksz;
+      char *new_data = (char *)realloc(data_, new_size);
+      if (!new_data)
+        throw std::runtime_error("ERROR: failed to realloc PackBuff!");
+
+      // fix relocations which are inside the old buffer!
+      for (auto it = fixup_.begin(); it != fixup_.end(); ++it) {
+        auto address = *it;
+        if ((char *)address >= data_ && (char *)address < (data_ + size_)) {
+          size_t offset = (char *)address - data_;
+          *it = (void **)(new_data + offset);
+        }
+      }
+      data_ = new_data;
+      size_alloc = new_size;
+    }
+    if (buff) {
+      // copy buffer to data buffer
+      memcpy((void *)(data_ + asize), (const void *)buff, len);
+    }
+    // mark fixup address
+    *address = (void *)asize;
+    fixup_.push_back(address);
+    size_ = asize + len;
+  }
+  size_t size() {
+    return size_;
+  }
+  void fixup(uint64_t VE_addr) {
+    for (auto it = fixup_.begin(); it != fixup_.end(); ++it) {
+      auto address = *it;
+      *address = (void *)((uint64_t)(*address) + VE_addr);
+    }
+  }
+  void *data() {
+    return (void *)(data_);
+  }
+};
+
+#endif // PACKBUFF_H_
