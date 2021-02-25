941745f8b35f10c5f459933c0ccfaaf1990f793c
Erich Focht <efocht@gmail.com>
2021-02-25 19:17:49 +0100

    Multi-process / multi-VE mode. Results are not looking correct, yet, on other processes than the first.

--- device/libkernel_ga.c
+++ device/libkernel_ga.c
@@ -3,6 +3,8 @@
 #include "device_args.h"
 #include "lga.h"
 
+#define MIN(a,b) ((a)<(b)?(a):(b))
+
 /*
 IC:  initial calculation of energy of populations
 GG:  genetic generation
@@ -15,95 +17,33 @@ LS:  local search
 uint64_t libkernel_ga (
                        struct device_args *da
                        )
-#if 0
-	const 	float*		PopulationCurrentInitial,
-			uint64_t  	VEVMA_PopulationCurrentFinal,
-			uint64_t  	VEVMA_EnergyCurrent,
-			uint64_t	VEVMA_Evals_performed,
-            uint64_t	VEVMA_Gens_performed,
-			uint*    	dockpars_prng_states,
-			uint		DockConst_pop_size,
-		    uint        DockConst_num_of_energy_evals,
-			uint        DockConst_num_of_generations,
-		    float       DockConst_tournament_rate,
-			float       DockConst_mutation_rate,
-		    float       DockConst_abs_max_dmov,
-			float       DockConst_abs_max_dang,
-		    float       Host_two_absmaxdmov,
-			float       Host_two_absmaxdang,
-			float       DockConst_crossover_rate,
-			uint        DockConst_num_of_lsentities,
-			uchar       DockConst_num_of_genes,
-	// pc
-	const	int* 		PC_rotlist,
-	const	float*		PC_ref_coords_x,// TODO: merge them into a single one?
-	const	float*		PC_ref_coords_y,
-	const	float*		PC_ref_coords_z,
-	const	float*		PC_rotbonds_moving_vectors,
-	const	float*		PC_rotbonds_unit_vectors,
-	const	float*		PC_ref_orientation_quats,
-        uint		DockConst_rotbondlist_length,
-	// ia
-	const 	float*		IA_IE_atom_charges,
-	const	int*		IA_IE_atom_types,
-	const	int*		IA_intraE_contributors,
-	const	float*		IA_reqm,
-	const	float*		IA_reqm_hbond,
-	const	uint*		IA_atom1_types_reqm,
-	const	uint*		IA_atom2_types_reqm,
-	const	float*		IA_VWpars_AC,
-	const	float*		IA_VWpars_BD,
-	const	float*		IA_dspars_S,
-	const	float*		IA_dspars_V,
-			float		DockConst_smooth,
-			uint		DockConst_num_of_intraE_contributors,
-			float		DockConst_grid_spacing,
-			uint		DockConst_num_of_atypes,
-			float		DockConst_coeff_elec,
-			float		DockConst_qasp,
-			float		DockConst_coeff_desolv,
-	// ie
-	const	uint64_t	VEVMA_Fgrids,
-			uchar		DockConst_g1,
-			uint		DockConst_g2,
-			uint		DockConst_g3,
-			uchar		DockConst_num_of_atoms,
-			float		DockConst_gridsize_x_minus1,
-			float		DockConst_gridsize_y_minus1,
-			float		DockConst_gridsize_z_minus1,
-			uint		Host_mul_tmp2,
-			uint		Host_mul_tmp3,
-	// ls
-			ushort		DockConst_max_num_of_iters,
-			float		DockConst_rho_lower_bound,
-			float		DockConst_base_dmov_mul_sqrt3,
-			float		DockConst_base_dang_mul_sqrt3,
-			uchar		DockConst_cons_limit,
-	// Values changing every LGA run
-            uint		Host_num_of_runs
-)
-#endif
 {
 	uint DockConst_pop_size = da->DockConst_pop_size;
 	uint Host_num_of_runs = da->Host_num_of_runs;
+        int ve_proc_id = da->ve_proc_id;
+        int ve_num_procs = da->ve_num_procs;
 
 	// initialize random generator with seed passed from host
-	randf_vec_init(&(da->dockpars_prng_states)[0], DockConst_pop_size);
+	randf_vec_init(&(da->dockpars_prng_states)[ve_proc_id * DockConst_pop_size], DockConst_pop_size);
 
+	int ve_num_of_runs = (Host_num_of_runs + ve_num_procs - 1) / ve_num_procs;
+
+	unsigned int run_cnt_start = ve_proc_id * ve_num_of_runs;
+	unsigned int run_cnt_end = MIN((ve_proc_id + 1) * ve_num_of_runs, Host_num_of_runs);
 
 #pragma omp parallel for schedule(static, 1)
-	for (unsigned int run_cnt = 0; run_cnt < Host_num_of_runs; run_cnt++) {
+	for (unsigned int run_cnt = run_cnt_start; run_cnt < run_cnt_end; run_cnt++) {
 
 /*
 		printf(" %u", run_cnt+1); 
 		fflush(stdout);
 */
 		// Values changing every LGA run
-		unsigned int uint_run_cnt  = run_cnt;
-		unsigned int Host_Offset_Pop = run_cnt * DockConst_pop_size* ACTUAL_GENOTYPE_LENGTH;
-		unsigned int Host_Offset_Ene = run_cnt * DockConst_pop_size;
+		unsigned int uint_run_cnt  = run_cnt - run_cnt_start;
+		unsigned int Host_Offset_Pop = uint_run_cnt * DockConst_pop_size * ACTUAL_GENOTYPE_LENGTH;
+		unsigned int Host_Offset_Ene = uint_run_cnt * DockConst_pop_size;
 
-        lga(
+		lga(
 			da->PopulationCurrentInitial,
 			da->PopulationCurrentFinal,
 			da->EnergyCurrent,
