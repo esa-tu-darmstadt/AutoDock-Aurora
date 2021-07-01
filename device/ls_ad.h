94934b6f4469e9065a786217e5697cd92f57bec2
Leonardo Solis V <leonardosolis86@gmail.com>
2021-07-01 15:01:28 +0200

    Adding (very) preliminary definition of ADADELTA kernel.

---
+++ device/ls_ad.h
@@ -0,0 +1,18 @@
+#ifndef _INCLUDE_LOCALSEARCH_ADADELTA_H_
+#define _INCLUDE_LOCALSEARCH_ADADELTA_H_
+
+#include "rand_gen.h"
+
+/*
+ * Ported from AutoDock-GPU v1.2
+ * https://github.com/ccsb-scripps/AutoDock-GPU/blob/eed190fd08844f5a1336cf36e267d7be3d8a6b61/device/kernel_ad.cl
+ */
+
+void ls_ad(
+    uint DockConst_num_of_atoms,    // FIXME: sw defines it as uchar
+
+
+
+);
+
+#endif
