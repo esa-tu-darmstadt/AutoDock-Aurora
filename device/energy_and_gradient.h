cdf5e7a1f23dee8790cbd56a8d42728c06aaa695
Leonardo Solis V <leonardosolis86@gmail.com>
2021-07-06 00:06:43 +0200

    Adding new function prototype.

---
+++ device/energy_and_gradient.h
@@ -0,0 +1,50 @@
+#ifndef _INCLUDE_ENERGY_AND_GRADIENT_H
+#define _INCLUDE_ENERGY_AND_GRADIENT_H
+
+void energy_and_gradient (
+	const	uint 				DockConst_pop_size,
+			float				local_coords_x[][MAX_POPSIZE],
+			float				local_coords_y[][MAX_POPSIZE],
+			float 				local_coords_z[][MAX_POPSIZE],
+	// ie
+	const 	float* 	restrict	IE_Fgrids,
+	const 	float*	restrict	IA_IE_atom_charges,
+	const	int*	restrict	IA_IE_atom_types,
+			uchar 				DockConst_xsz,
+			uchar 				DockConst_ysz,
+			uchar 				DockConst_zsz,
+			uchar 				DockConst_num_of_atoms,	// TODO: FIXME
+			float 				DockConst_gridsize_x_minus1,
+			float 				DockConst_gridsize_y_minus1,
+			float 				DockConst_gridsize_z_minus1,
+			uint 				Host_mul_tmp2,
+			uint 				Host_mul_tmp3,
+	// ia
+	const 	int*	restrict	IA_intraE_contributors,
+	const	float*	restrict	IA_reqm,
+	const 	float*	restrict	IA_reqm_hbond,
+	const 	uint*	restrict	IA_atom1_types_reqm,
+	const	uint*	restrict	IA_atom2_types_reqm,
+	const	float*	restrict	IA_VWpars_AC,
+	const	float*	restrict	IA_VWpars_BD,
+	const	float*	restrict	IA_dspars_S,
+	const 	float*	restrict	IA_dspars_V,
+			float				DockConst_smooth,
+			uint 				DockConst_num_of_intraE_contributors,
+			float 				DockConst_grid_spacing
+			uint 				DockConst_num_of_atypes,
+			float				DockConst_coeff_elec,
+			float				DockConst_qasp,
+			float				DockConst_coeff_desolv,
+    // gradients
+    const   int*    restrict    GRAD_rotbonds,
+    const   int*    restrict    GRAD_rotbonds_atoms,
+    const   int*    restrict    GRAD_num_rotating_atoms_per_rotbond,
+
+    const   float*  restrict    GRAD_angle,
+    const   float*  restrict    GRAD_dependence_on_theta,
+    const   float*  restrict    GRAD_dependence_on_rotangle	
+);
+
+
+#endif
